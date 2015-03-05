790bec7e80a3d9a8b239914830019ef36d4d2763
Peter Varshavsky <pvarsh@gmail.com>
2015-03-04 22:05:40 -0500

    Adds file.

---
+++ LKvar_tester.m
@@ -0,0 +1,27 @@
+% ------------------------------------------------
+% Testing BicubicSR.m
+
+n_channels = 3 % use 1 to only keep first channel or 3 to keep all 3 in RGB
+
+im_file = '/Users/petervarshavsky/Dropbox//NYU/superresolution/data/meklit/meklit.png';
+img = imread(im_file, 'png');
+img = img(:,:,1:n_channels); % only keep one channel
+imshow(img);
+
+disp('Image size: ');
+disp(size(img));
+
+SR_factor = 2 % magnification
+[H, W] = size(img); % Height and Width
+
+options.L = 1;
+options.res = SR_factor;
+options.n = H;
+options.m = W;
+options.N = options.n * SR_factor;
+options.M = options.m * SR_factor;
+options.Real = true;
+
+disp('Size of img before BicubicSR: ');
+disp(size(img));
+
