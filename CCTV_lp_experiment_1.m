22291bd989d2c51aa4e75d2b5b1a68ff87802faf
Peter Varshavsky <pvarsh@gmail.com>
2015-03-23 18:16:46 -0400

    TV_SAR experiment

--- CCTV_lp_experiment_1.m
+++ CCTV_lp_experiment_1.m
@@ -1,13 +1,123 @@
+opt.filepath = '/Users/petervarshavsky/Dropbox/NYU/superresolution/data/CCTV-robbery-youtube/extracted_sequences/license_plate';
+opt.outpath = '/Users/petervarshavsky/Dropbox/NYU/superresolution/output/villena/CCTV_lp_0321';
 
+%% No blur. Bicubic.
+opt.sr_method = 1;
+opt.blur_method = 1;
+opt.blur_size = 'None';
+opt.blur_var = 'None';
+opt.lambda_prior = 'None';
+sr_cli(opt);
 
-inpath = '/Users/petervarshavsky/Dropbox/NYU/superresolution/data/CCTV-robbery-youtube/extracted_sequences/license_plate'
-outpath = '/Users/petervarshavsky/Dropbox/NYU/superresolution/output/villena/CCTV_lp_0321'
+%%------------------- SAR-TV
+opt.sr_method = 6;
+opt.blur_method = 1;
+opt.blur_size = 'None';
+opt.blur_var = 'None';
+opt.lambda_prior = 0;
+sr_cli(opt);
 
-% blurs = {1,2,3,4}
-% sr_methods = {1,2,3,4}
+opt.sr_method = 6;
+opt.blur_method = 2;
+opt.blur_size = 3;
+opt.blur_var = 1;
+opt.lambda_prior = 0;
+sr_cli(opt);
 
-blur = 2
-blur_size = 3
-sr_method = 2
+opt.sr_method = 6;
+opt.blur_method = 2;
+opt.blur_size = 3;
+opt.blur_var = 3;
+opt.lambda_prior = 0;
+sr_cli(opt);
 
-sr_cli(inpath, outpath, sr_method, blur, blur_size)
+opt.sr_method = 6;
+opt.blur_method = 2;
+opt.blur_size = 5;
+opt.blur_var = 1;
+opt.lambda_prior = 0;
+sr_cli(opt);
+
+opt.sr_method = 6;
+opt.blur_method = 2;
+opt.blur_size = 5;
+opt.blur_var = 3;
+opt.lambda_prior = 0;
+sr_cli(opt);
+
+% lambda .5
+opt.sr_method = 6;
+opt.blur_method = 1;
+opt.blur_size = 'None';
+opt.blur_var = 'None';
+opt.lambda_prior = .5;
+sr_cli(opt);
+
+opt.sr_method = 6;
+opt.blur_method = 2;
+opt.blur_size = 3;
+opt.blur_var = 1;
+opt.lambda_prior = .5;
+sr_cli(opt);
+
+opt.sr_method = 6;
+opt.blur_method = 2;
+opt.blur_size = 3;
+opt.blur_var = 3;
+opt.lambda_prior = .5;
+sr_cli(opt);
+
+opt.sr_method = 6;
+opt.blur_method = 2;
+opt.blur_size = 5;
+opt.blur_var = 1;
+opt.lambda_prior = .5;
+sr_cli(opt);
+
+opt.sr_method = 6;
+opt.blur_method = 2;
+opt.blur_size = 5;
+opt.blur_var = 3;
+opt.lambda_prior = .5;
+sr_cli(opt);
+
+% lambda .5
+
+% lambda 1
+opt.sr_method = 6;
+opt.blur_method = 1;
+opt.blur_size = 'None';
+opt.blur_var = 'None';
+opt.lambda_prior = 1;
+sr_cli(opt);
+
+opt.sr_method = 6;
+opt.blur_method = 2;
+opt.blur_size = 3;
+opt.blur_var = 1;
+opt.lambda_prior = 1;
+sr_cli(opt);
+
+opt.sr_method = 6;
+opt.blur_method = 2;
+opt.blur_size = 3;
+opt.blur_var = 3;
+opt.lambda_prior = 1;
+sr_cli(opt);
+
+opt.sr_method = 6;
+opt.blur_method = 2;
+opt.blur_size = 5;
+opt.blur_var = 1;
+opt.lambda_prior = 1;
+sr_cli(opt);
+
+opt.sr_method = 6;
+opt.blur_method = 2;
+opt.blur_size = 5;
+opt.blur_var = 3;
+opt.lambda_prior = 1;
+sr_cli(opt);
+
+% lambda .5
+
